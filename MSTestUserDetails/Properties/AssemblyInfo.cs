c3946f58ac5172c141bc13d219185fed558f4f05
Pallavikothawade <Pallavijkothawade@gmail.com>
2023-01-17 17:56:07 +0530

    Add The First Name

---
+++ MSTestUserDetails/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MSTestUserDetails")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MSTestUserDetails")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d84ee3fd-58c6-485f-b1e1-34e9dfd3dcc3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ MSTestUserDetails/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using UserRegistrationMSTestDemo;
+
+namespace MSTestUserDetails
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+       [DataRow  (new string[] {"Pallavi","Akanksha","Ankita","Priyansh"},true)]
+       [DataRow(new string[] { "pallavi", "akanksha", "ankita", "priyansh" }, false)]
+
+        public void TestMethod1(string[] arr ,bool expected)
+        {
+
+           UserDetails userDetails = new UserDetails();
+            foreach(string s in arr)
+            {
+                bool actual= userDetails.FirstNameTestcase(s);
+                Assert.AreEqual(actual,expected);
+            }
+          
+        }
+    }
+}
